ab79408f1c0ace686a80ab9eb8ec7279e0fe807d
Saivelin <vlad.pochkalov@mail.ru>
2022-04-12 07:35:48 +0300

    Проработка еще 2-х вариантов ответа

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import telebot
 import TOKEN
 from telebot import types
+import qrcode
 
 bot = telebot.TeleBot(TOKEN.token)
 
@@ -18,6 +19,20 @@ def start_message(message):
 def func(message):
     if(message.text == "Нет, мне нет 18"):
         bot.send_message(message.chat.id, 'Тогда заходи, когда вырастешь)')
+    elif(message.text == "Да, мне есть 18"):
+        text = 'Окей. Ты согласен на обработку персональных данных?'
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.InlineKeyboardButton("Согласен")
+        button2 = types.InlineKeyboardButton("Не согласен")
+        markup.add(button1, button2)
+        bot.send_message(message.chat.id, text, reply_markup=markup)
+    elif(message.text == "Согласен"):
+        text = 'Хорошо, сейчас зарегестрирую тебя'
+        bot.send_message(message.chat.id, text, reply_markup=markup)
+        # Код регистрации
+        text = 'Регистрация прошла успешно) ВОт твой qr код, ИНФА ПРО QR КОД И ЕГО РАБОТУ'
+        bot.send_message(message.chat.id, text, reply_markup=markup)
+        #bot.send_photo(message.chat.id, )
 
 
 bot.polling(none_stop=True)
